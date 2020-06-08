1723c18f45236d70be632b99b514bb83016fa8f0
Sarah Peters <sarahpieters15@gmail.com>
2020-06-08 10:04:42 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words)
+  counter = 0
+
+  while counter < words.length
+    yield(words[counter])
+    counter += 1
+  end
+
+words
 end
